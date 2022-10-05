1ce878261d75b21376f1ce51218f90d4a99a638c
pupil <pupil@ivkhk.ee>
2022-10-05 14:34:37 +0300

    second commit

--- src/jptv21oopproba/JPTV21OOPproba.java
+++ src/jptv21oopproba/JPTV21OOPproba.java
@@ -5,6 +5,9 @@
  */
 package jptv21oopproba;
 
+import myclasses.Author;
+import myclasses.Book;
+
 /**
  *
  * @author pupil
@@ -15,7 +18,35 @@ public class JPTV21OOPproba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Author author1 = new Author();
+        author1.setFirstname("Aleksandr");
+        author1.setLastname("Pushkin"); 
+        Book book = new Book();
+        Author[]  authors = new Author[1];
+        authors[0] = author1;
+        book.setAuthors(authors);
+         System.out.println(book.toString());
+        
+        Book book2 = new Book();
+        book2.setCaption("Zolotoy telyonok");
+        Author author2 = new Author();
+        author2.setFirstname("Ilja");
+        author2.setLastname("Ilf");
+        Author author3 = new Author();
+        author3.setFirstname("Evgeny");
+        author3.setLastname("Petrov");
+        Author[] authors2 = new Author[2];
+        authors2[0] = author2;
+        authors2[1] = author3;
+        book.setAuthors(authors2);
+        System.out.println(book.toString());
+        System.out.println("+++++++++++++++++");
+        Book book3 =new Book();
+        book3.setCaption("kniga");
+        book3.addAuthor(author1);
+        book3.addAuthor(author2);
+        book3.addAuthor(author3);
+        System.out.println(book3.toString());
     }
     
 }
---
+++ src/myclasses/Book.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author pupil
+ */
+public class Book {
+    private Author[] authors = new Author[0];
+    private String caption;
+
+    public Book() {
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+    public void addAuthor(Author author){
+        int countAuthors = this.authors.length;
+        
+        Author[] newAuthors = Arrays.copyOf(this.authors, countAuthors+1);
+        newAuthors[countAuthors] = author;
+        this.authors =newAuthors;
+    
+    
+    }
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{"
+                + "authors=" + authors + Arrays.toString(authors)
+                + ", caption=" + caption 
+                + '}';
+    }
+}
